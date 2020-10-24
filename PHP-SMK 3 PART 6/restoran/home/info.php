501c2d8b38d15eda1d85cd7c44594c80a1909420
VirgiLesmana <68315824+VirgiLesmana@users.noreply.github.com>
2020-10-24 10:06:46 +0700

    Add files via upload

---
+++ PHP-SMK 3 PART 6/restoran/home/info.php
@@ -0,0 +1,4 @@
+<h3>REGISTRASI ANDA TELAH BERHASIL SILAHKAN LOGIN</h3><br>
+<div class="float-left mr-4">
+    <a class="btn btn-primary" href="?f=home&m=login" role="button">LOGIN</a>
+</div>
---
+++ PHP-SMK 3 PART 6/restoran/orderdetail/select.php
@@ -0,0 +1,1 @@
+<h1>Select OrderDetail</h1>
